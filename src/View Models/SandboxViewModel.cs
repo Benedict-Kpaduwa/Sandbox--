e9e7d17ecfa94d7238f2d6a70a3f94c4a7236a82
Christopher Atkinson <c_atkinson@outlook.com>
2022-01-12 21:43:21 +0000

    Reorganised functions to match view

--- src/View Models/SandboxViewModel.cs
+++ src/View Models/SandboxViewModel.cs
@@ -12,20 +12,20 @@ public class SandboxViewModel
         SandboxModel = new SandboxModel();
     }
 
-    public ICommand browse_command;
-
-    public ICommand OnBrowseCommand
-    {
-        get { return browse_command ??= new BrowseCommand(this); }
-        set { browse_command = value; }
-    }
-
     private ICommand clear_command;
 
     public ICommand OnClearCommand
     {
         get { return clear_command ??= new ClearCommand(this); }
         set { clear_command = value; }
+    }
+
+    public ICommand browse_command;
+
+    public ICommand OnBrowseCommand
+    {
+        get { return browse_command ??= new BrowseCommand(this); }
+        set { browse_command = value; }
     }
 
     private ICommand run_command;
